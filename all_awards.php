fdfa0a655709bdb48c13c0d125cfc534d96c6410
Lena <lenalin9977@gmail.com>
2020-11-19 10:06:14 +0800

    製作單期全部對獎程式之撈出要對獎的發票及該期開獎獎號

---
+++ all_awards.php
@@ -0,0 +1,35 @@
+<?php
+include_once "base.php";
+
+$period_str = [
+  1 => '1,2月',
+  2 => '3,4月',
+  3 => '5,6月',
+  4 => '7,8月',
+  5 => '9,10月',
+  6 => '11,12月'
+];
+
+echo "你要對的發票是".$_GET['year']."年";
+echo $period_str[$_GET['period']]."的發票";
+
+//撈出該期的發票
+$sql= "select * from `invoices` where `period`='{$_GET['period']}' && LEFT(date,4)='{$_GET['year']}' Order by date desc";
+$invoices = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+echo count($invoices);
+// echo "<pre>";
+// print_r($invoices);
+// echo "</pre>";
+
+
+//撈出該期的開講獎號
+$sql="select * from `award_numbers` where `period`='{$_GET['period']}' && `year`='{$_GET['year']}'";
+$awNums = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($invoices);
+echo "</pre>";
+
+//開始對獎
+
+?>
+
